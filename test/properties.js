0ef56faeaf73fc0f7fd5d8897bb6415d2ddeadc0
Conny Brunnkvist <cbrunnkvist@gmail.com>
2016-10-12 18:12:13 +0700

    Throw on mutation, present amount as string

--- test/properties.js
+++ test/properties.js
@@ -4,13 +4,22 @@ const Money = require('../')
 describe('Money object', () => {
   before(() => {
     this.oneUSD = new Money('USD', 1)
+    this.oneVND = new Money('VND', 1)
   })
 
-  it('only exposes currency code and amount', () => {
+  it('only exposes currency and amount', () => {
     const props = Object.keys(this.oneUSD).sort()
     const expected = ['amount', 'currency']
     assert.deepEqual(props, expected)
   })
 
-  it('treats amount as string')
+  it('throws an error if trying to mutate either currency or amount', () => {
+    assert.throws(() => { this.oneUSD.amount = 0 }, Error, 'amount')
+    assert.throws(() => { this.oneUSD.currency = '' }, Error, 'currency')
+  })
+
+  it('presents the amount as a string with correct precision', () => {
+    assert.strictEqual(this.oneUSD.amount, '1.00')
+    assert.strictEqual(this.oneVND.amount, '1')
+  })
 })
